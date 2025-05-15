3b7cd3b731f37d2dae7542b935f2f7cbf0dd3a16
art-Artur Ribeiro <profissional.arturrc@gmail.com>
2025-05-15 01:07:43 -0300

    Conclusão da base do SignInView

--- Habits/SignIn/View/SignInView.swift
+++ Habits/SignIn/View/SignInView.swift
@@ -6,28 +6,50 @@ struct SignInView: View {
     
     @State var email: String = ""
     @State var password: String = ""
-    
     @State var action: Int? = 0
+    @State var navigationHidden: Bool = true
     
     var body: some View {
         NavigationView {
             ScrollView (showsIndicators: false){
-                VStack(alignment: .center) {
+                VStack(alignment: .center, spacing: 20) {
+                    Spacer(minLength: 36)
+                    VStack(alignment: .center, spacing: 8) {
+            
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal,48)
+                        
+                        Text("Login")
+                            .foregroundColor(Color("ColorPrimary"))
+                            .font(Font.system(.title).bold())
+                        
+                        numberField
+                        passwordField
+                        enterButton
+                        registerLink
+                        
+                        Text("Copyrights @ sei la")
+                            .foregroundColor(Color.gray)
+                            .font(Font.system(size: 12).bold())
+                            .padding(.top, 16)
+                    }
+                }.frame (
+                    maxWidth: .infinity,
+                    maxHeight: .infinity
                     
-                    Image("logo")
-                        .resizable()
-                        .scaledToFit()
-                        .padding(.horizontal,48)
-                    
-                    Text("Login")
-                        .foregroundColor(Color("ColorPrimary"))
-                        .font(Font.system(.title).bold())
-                    
-                    numberField
-                    passwordField
-                    enterButton
-                    registerLink
-                }
+                ).padding (
+                    .horizontal, 24)
+                .background(Color.white)
+                .navigationBarTitle(
+                    "Login",
+                    displayMode: .inline )
+                .navigationBarHidden(navigationHidden)
+            }.onAppear {
+                self.navigationHidden = true
+            }.onDisappear{
+                self.navigationHidden = false
             }
         
         }
@@ -41,23 +63,17 @@ extension SignInView {
         TextField("Email", text: $email)
             .border(Color.colorPrimary)
     }
-}
-
-extension SignInView {
+    
     var passwordField: some View {
         SecureField("Senha", text: $password)
             .border(Color.colorPrimary)
     }
-}
-
-extension SignInView {
+    
     var enterButton: some View {
         Button("Entrar"){
         }
     }
-}
-
-extension SignInView {
+    
     var registerLink: some View {
         VStack{
             Text("Ainda não possui uma conta?")
@@ -80,8 +96,6 @@ extension SignInView {
         }
     }
 }
-
-
 
 #Preview {
     let viewModel = SignInViewModel()
